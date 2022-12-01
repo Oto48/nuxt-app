49fd1871a2fa6a897a9fe5a31326e319de25c879
oto48 <otchokhonelidze4@gmail.com>
2022-12-01 11:50:55 +0400

    add login and signup page

---
+++ pages/login/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <form>
+        <label>Email</label>
+        <input type="email" v-model="email">
+        <label>Password</label>
+        <input type="password" v-model="password">
+        <button @click="loginHandler">Log In</button>
+    </form>
+</template>
+  
+<script>
+export default {
+    name: 'Login',
+    data() {
+        return {
+            email: null,
+            password: null
+        }
+    },
+    methods: {
+        loginHandler() {
+            const login = {'email': this.email, 'password': this.password}
+            console.log(login)
+        }
+    }
+}
+</script>
+  
