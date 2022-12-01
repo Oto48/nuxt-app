c3e85b1b03f7778fa5c327eeb9e74f912900322e
oto48 <otchokhonelidze4@gmail.com>
2022-12-01 23:59:52 +0400

    fix logout function

--- components/Header.vue
+++ components/Header.vue
@@ -1,13 +1,16 @@
 <template>
     <nav>
-        <NuxtLink to="/login">Login</NuxtLink>
-        <h1 v-if="true">{{$store.state.token}}</h1>
-        <!-- <NuxtLink v-if="$store.state.users.length" to="/login">Login</NuxtLink> -->
-        <!-- <NuxtLink v-else to="/">Login</NuxtLink> -->
-        <NuxtLink to="/signup">Signup</NuxtLink>
-        <li @click="logOut">
-            <NuxtLink to="/">Logout</NuxtLink>
-        </li>
+        <ul>
+            <li v-if="$store.state.token == 'false'">
+                <NuxtLink to="/login">Login</NuxtLink>
+            </li>
+            <li v-else="$store.state.token == 'true'" @click="logOut">
+                <NuxtLink to="/">Logout</NuxtLink>
+            </li>
+            <li>
+                <NuxtLink to="/signup">Signup</NuxtLink>
+            </li>
+        </ul>
     </nav>
 </template>
 
@@ -16,26 +19,9 @@ export default {
     name: 'Header',
     methods: {
         logOut() {
-            // this.token = false;
-            // localStorage.setItem("token", this.token);
-            // this.$store.commit('ADD_USER', localStorage.getItem("token"));
-            // console.log(this.token);
-
-
-            localStorage.setItem("token", false);
-            this.$store.commit('editToken', false);
+            localStorage.setItem("token", 'false');
+            this.$store.commit('editToken', 'false');
             console.log(this.token);
-
-
-            // const user = {'name': this.name, 'password': this.password};
-            // let localItems = JSON.parse(localStorage.getItem("users"));
-            // localItems === null ? 'undefined':
-            // localItems.forEach((x) => {
-            //     if(JSON.stringify(x) == JSON.stringify(user)) {
-            //         localStorage.setItem("token", true);
-            //         this.$router.push('/home');
-            //     }
-            // }); 
         }
     }
 }
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -27,8 +27,8 @@ export default {
             localItems === null ? 'undefined':
             localItems.forEach((x) => {
                 if(JSON.stringify(x) == JSON.stringify(user)) {
-                    localStorage.setItem("token", true);
-                    this.$store.commit('editToken', true);
+                    localStorage.setItem("token", 'true');
+                    this.$store.commit('editToken', 'true');
                     this.$router.push('/home');
                 }
             }); 
