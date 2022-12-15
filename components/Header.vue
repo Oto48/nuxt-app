92f63e4d0dc99b3feeb7f814d5796754c179577a
oto48 <otchokhonelidze4@gmail.com>
2022-12-15 18:16:11 +0400

    add responsive header

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,16 @@
 <template>
     <nav class="bg-black">
-        <ul>
+        <div class="hamburger"
+            :class="{ active: showMobileMenu }"
+            @click="showMobileMenu = !showMobileMenu"
+        >
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+        <ul class="nav-menu"
+            :class="{ 'active bg-black': showMobileMenu }"
+        >
             <li v-if="$store.state.token.role">
                 <NuxtLink to="/home">Home</NuxtLink>
             </li>
@@ -34,13 +44,25 @@ export default {
     data() {
         return {
             user: localStorage.getItem("token") ? JSON.parse(localStorage.getItem("token")) : "",
+            showMobileMenu: false,
         }
     },
     methods: {
         logOut() {
             localStorage.setItem("token", '');
             this.$store.commit('editToken', '');
+        },
+        myEventHandler(e) {
+            if(e.currentTarget.innerWidth > 768){
+                this.showMobileMenu = false;
+            }
         }
+    },
+    created() {
+        window.addEventListener("resize", this.myEventHandler);
+    },
+    destroyed() {
+        window.removeEventListener("resize", this.myEventHandler);
     }
 }
 </script>
@@ -49,9 +71,58 @@ export default {
 nav {
     display: flex;
     justify-content: space-between;
+    align-items: center;
     padding: 5px 25px;
     font-weight: bold;
     font-size: 20px;
+}
+
+.hamburger {
+    display: none;
+    cursor: pointer;
+}
+
+.bar {
+    display: block;
+    width: 25px;
+    height: 3px;
+    margin: 5px auto;
+    -webkit-transition: all 0.3 ease-in-out;
+    transition: all 0.3s ease-in-out;
+    background: white;
+}
+
+@media(max-width: 768px) {
+    .hamburger {
+        display: block;
+    }
+    .hamburger.active .bar:nth-child(2) {
+        opacity: 0;
+    }
+    .hamburger.active .bar:nth-child(1) {
+        transform: translateY(8px) rotate(45deg);
+    }
+    .hamburger.active .bar:nth-child(3) {
+        transform: translateY(-8px) rotate(-45deg);
+    }
+    .nav-menu {
+        position: fixed;
+        left: -100%;
+        top: 53px;
+        gap: 0;
+        flex-direction: column;
+        text-align: center;
+        padding: 30px 0px;
+        width: 100%;
+        transition: 0.3s;
+        gap: 15px;
+    }
+    .nav-item {
+        margin: 16px 0;
+    }
+    .nav-menu.active {
+        left: 0;
+    }
 }
 
 ul {
@@ -66,4 +137,11 @@ a {
     text-decoration: none;
     color: white;
 }
+
+a:hover {
+    color: #FFD700;
+    text-shadow: 0 0 3px #ffef93;
+    transition: 0.3s;
+}
+
 </style>
